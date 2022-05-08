5e4e11608472de6fb2f84c0c18289d33e24f87cc
Ahmad Rafi Wirana <ahmad.rafi02@ui.ac.id>
2022-05-08 14:14:38 +0700

    Initial Commit

---
+++ memorize app/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  memorize app
+//
+//  Created by Ahmad Rafi Wirana on 08/05/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ memorize app/memorize_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  memorize_appApp.swift
+//  memorize app
+//
+//  Created by Ahmad Rafi Wirana on 08/05/22.
+//
+
+import SwiftUI
+
+@main
+struct memorize_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
